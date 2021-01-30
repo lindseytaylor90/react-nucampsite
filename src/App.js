d3459556fc865e83d3e0d4e6a301114d36155afe
Lindseytaylor90 <tylindsey90@gmail.com>
2021-01-29 18:36:02 -0700

    components part1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
 
 
@@ -12,6 +13,7 @@ class App extends Component {
                 <NavbarBrand href="/">Nucamp</NavbarBrand>
             </div>
           </Navbar>
+          <Directory />
       </div>
     );
   }
