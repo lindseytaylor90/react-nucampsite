797ba4a5e04b601e66489d0ff6d275f948f69692
Lindseytaylor90 <tylindsey90@gmail.com>
2021-01-31 16:15:35 -0700

    Functional Components

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+function RenderCampSite({campsite}){
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+
+function RenderComments({comments}) {
+    if(comments){
+        return (
+            <div className="col-md-5 m-1">
+                <h4>Comments</h4>
+                {comments.map(c => {
+                    return(
+                        <div>
+                            <p>{c.text}</p>
+                            <p>
+                                {c.author},
+                                {" "+ new Intl.DateTimeFormat(
+                                    'en-US', 
+                                    { 
+                                        year: 'numeric', 
+                                        month: 'short', 
+                                        day: '2-digit'
+                                    }
+                                    ).format(new Date(Date.parse(c.date)))}
+                            </p>
+                        </div>
+                    );
+                })}
+            
+            </div>
+        );
+    }
+    return <div/>
+}
+
+    function CampsiteInfo(props) {
+        if (props.campsite) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <RenderCampSite campsite={props.campsite} />
+                        <RenderComments comments={props.campsite.comments} />
+                    </div>
+                </div>
+            );
+        } 
+        return <div/>;
+    }
+
+
+
+export default CampsiteInfo;
