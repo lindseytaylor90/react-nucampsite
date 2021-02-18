a428b8304c0bcb9d6dff9cb84e6e2566b7740cff
Lindseytaylor90 <tylindsey90@gmail.com>
2021-02-17 19:38:40 -0700

    loading component

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label,Col,Row  } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label,Col,Row, } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { Loading } from './LoadingComponent';
  
 function RenderCampSite({campsite}){
     return (
@@ -48,7 +48,28 @@ function RenderComments({comments, addComment, campsiteId }) {
     return <div/>
 }
 
-    function CampsiteInfo(props) {
+        function CampsiteInfo(props) {
+            if (props.isLoading) {
+                return (
+                    <div className="container">
+                        <div className="row">
+                            <Loading />
+                        </div>
+                    </div>
+                );
+            }
+            if (props.errMess) {
+                return (
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <h4>{props.errMess}</h4>
+                            </div>
+                        </div>
+                    </div>
+                );
+            }
+            
         if (props.campsite) {
             return (
                 <div className="container">
@@ -199,6 +220,5 @@ function RenderComments({comments, addComment, campsiteId }) {
             )
         }
     }
-    
 
 export default CampsiteInfo;
